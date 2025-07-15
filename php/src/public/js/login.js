bc1284b788364f43384af1a5f73ae3fde821c166
Luigi Basantes <luigi@jelou.ai>
2025-07-15 08:47:15 -0500

    fix: repository structure

--- php/src/public/js/login.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('copyBtn').onclick = async () => {
-  let text = document.getElementById('redirectURL').textContent;
-  await navigator.clipboard.writeText(text);
-
-  alert('Copied');
-};
