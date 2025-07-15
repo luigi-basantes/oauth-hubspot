e0a4464f5ac302c8f44e1c55f36ff9dc06202184
Luigi Basantes <luigi@jelou.ai>
2025-07-15 17:10:17 -0500

    fix: fixed proxy

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,13 @@
 const PORT = process.env.PORT || 3000;
+const NODE_ENV = process.env.NODE_ENV || "development";
+
+const REDIRECT_URI = NODE_ENV === "production" ? "https://oauth-hubspot.fly.dev/oauth-callback" : `http://localhost:${PORT}/oauth-callback`;
+
 module.exports = {
     PORT,
     OBJECTS_LIMIT: 30,
     SCOPES: "crm.objects.contacts.read",
-    REDIRECT_URI: `http://localhost:${PORT}/oauth-callback`,
+    REDIRECT_URI,
     CLIENT_ID: process.env.HUBSPOT_CLIENT_ID,
     CLIENT_SECRET: process.env.HUBSPOT_CLIENT_SECRET,
     GRANT_TYPES: {
